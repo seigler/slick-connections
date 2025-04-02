420d719fc1cbc8b83730da2b77761b7f3c75bdd1
Joshua Seigler <2583159+seigler@users.noreply.github.com>
2025-04-01 18:24:51 -0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { createSignal } from "solid-js";
+// import logo from "./assets/logo.svg";
+import "./App.css";
+
+function App() {
+
+  return (
+    <main class="container">
+      <h1>Slick Connections</h1>
+    </main>
+  );
+}
+
+export default App;
